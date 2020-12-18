d952b014f9ba5af64f3998895b76e0adeb853bfe
Smitchy <47454064+Smitchy@users.noreply.github.com>
2020-12-18 04:26:41 +0100

    most stuff done

--- script.js
+++
@@ -1,8 +0,0 @@
-AFRAME.registerComponent('random-color', {
-    init: function(){
-        this.el.addEventListener('collide', function(){
-            var randomHex = '#' + Math.random().toString(16).substr(-6);
-            this.el.setAttribute('material', 'color', randomHex);
-                                 });
-    }
-});
